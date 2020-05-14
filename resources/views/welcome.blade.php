45ccab67b8c3ec50bd041eacb1a98416b90774b8
taira.mizuki <rhcp14151415@gmail.com>
2020-05-14 09:32:43 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,6 +90,20 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
+        @extends('layouts.app')
+
+@section('content')
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    @endif
+@endsection
         </div>
     </body>
 </html>
